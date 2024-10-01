c50d13acf469f77f4628c0945d4dd4aedee9a85a
James Kelly <jk377y@gmail.com>
2024-10-01 18:17:07 -0500

    set up page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,9 @@
-import Home from './pages/Home';
-import Booking from './pages/Booking';
-import Services from './pages/Services';
-import Staff from './pages/Staff';
-import Admin from './pages/Admin';
+
 
 const App = () => {
   return (
     <>
-        <Home/>
-        <Booking/>
-        <Services/>
-        <Staff/>
-        <Admin/>
+        
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
+import Services from './pages/Services';
+import Staff from './pages/Staff';
+import Admin from './pages/Admin';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Router>
+        <Routes path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="booking" element={<Booking />} />
+            <Route path="services" element={<Services />} />
+            <Route path="staff" element={<Staff />} />
+            <Route path="admin" element={<Admin />} />
+        </Routes>
+    </Router>
+);
