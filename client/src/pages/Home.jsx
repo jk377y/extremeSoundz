4b24e644c57707769e4e00d7a2645c46aa1ebbac
James Kelly <jk377y@gmail.com>
2024-10-06 11:20:37 -0500

    added test link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const Home = () => {
+    function handleClick() {
+        console.log('The link was clicked. u r smart');
+    }
     return(
         <>
             <h1>This is the Home page.</h1>
+            <p>click the link, check dev tools in browser <a href="#" onClick={handleClick}>Click me</a></p>
         </>
     )
 }
